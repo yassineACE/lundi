b8270ec16ad871efc0e451f5f47b5524da23e267
yassineACE <yassinemedmoum453@gmail.com>
2023-09-18 11:41:55 +0100

    first lundi

---
+++ 2lundi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int   main ()
+{
+    int nombre ;
+    printf("entre un nombre ");
+    scanf("%d",&nombre);
+    if (nombre %2==0){
+        printf("le nombre est est paiar\n");
+    } else {
+    printf(" le nombre est impiar \n");
+
+    }
+
+
+}
